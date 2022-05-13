dd7fed9da68de696b7308fcb5e2eb67c484daf30
vsh <88970656+vshatgit@users.noreply.github.com>
2022-05-13 18:04:48 +0200

    rewrite

--- main.c
+++ main.c
@@ -4,15 +4,16 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define WIDTH 1200
-#define HEIGHT 1200
+#define WIDTH 600
+#define HEIGHT 800
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 typedef uint8_t Color[3];
-typedef Color Image[WIDTH][HEIGHT];
+typedef Color Image[HEIGHT][WIDTH];
 
 typedef struct Point {
     float x,y;
@@ -21,26 +22,26 @@ typedef struct Point {
 bool debug = false;
 
 void set_pixel_color(Image img, int px, int py, Color color) {
-    for(int i = 0; i < 3; i++) {
-	img[py][px][i] = color[i];
-    }
+    memcpy(img[py][px], color, sizeof(char)*3);
 }
 
 void set_pixel_color_center(Image img, int px, int py, Color color) {
-    //if (debug) printf("set_pixel: %d, %d\n", px,py);
-    set_pixel_color(img, px+HEIGHT/2, WIDTH/2-py, color);
+    set_pixel_color(img, px+HEIGHT/2, py+WIDTH/2, color);
 }
 
-void fill(Image img, Color color)
-{
-    for(int row = 0; row < HEIGHT; row++) {
-	for(int col = 0; col < WIDTH; col++) {
-	    set_pixel_color(img, row, col, color);
+void draw_rect(Image img, Color color, int lx, int ly, int x, int y) {
+    for(int row = 0; row < ly; row++) {
+	for(int col = 0; col < lx; col++) {
+	    set_pixel_color(img, col+x, row+y, color);
 	}
     }
 }
 
-void line(Image img, Color line_color, Point a, Point b) {
+void image_fill(Image img, Color color) {
+    draw_rect(img,color,WIDTH,HEIGHT,0,0);
+}
+
+void draw_line(Image img, Color line_color, Point a, Point b) {
     int x_distance = abs(a.x - b.x);
     int y_distance = abs(a.y - b.y);
     int distance = MAX(x_distance, y_distance);
@@ -66,33 +67,35 @@ void line(Image img, Color line_color, Point a, Point b) {
     Point curr;
     for(int i = 0; i < distance; i++) {
 	curr = points[i];
-	set_pixel_color_center(img, round(curr.x), round(curr.y), line_color);
+	set_pixel_color(img, round(curr.x), round(curr.y), line_color);
     }
 }
 
-void draw_parabola(Image img, float a, float b, float c) {
+void draw_quadratic(Image img, float a, float b, float c) {
     // Assuming the formula looks like this
     // f(x) = a(x*x) + bx + c
     assert(a != 0);
+    a *= -1; b *= -1; c *= -1;
     Color line_color = { 255, 105, 55 };
 
-    Point prev, curr = { 0, c };
-    int x = 1;
-    while(curr.y < HEIGHT/2 && curr.y > -HEIGHT/2) {
-	if (x > 1) {
-	    if (debug)
-		printf("drawing: (%f, %f) (%f, %f)\n", prev.x,prev.y,curr.x,curr.y);
-	    line(img, line_color, curr, prev);
+    float p = -1*b/(2*a);
+    float q = a*(p*p) + b*p + c;
+    Point curr = { p+WIDTH/2, q+HEIGHT/2 }, prev = { 0, 0 };
+
+    int x = p;
+    while((curr.y < HEIGHT && curr.y > 0) && (curr.x < WIDTH && curr.x > 0)) {
+	if (x > p) {
+	    draw_line(img, line_color, prev, curr);
 	    // mirror and draw the other side
-	    curr.x *= -1;
-	    prev.x *= -1;
-	    line(img, line_color, curr, prev);
-	    curr.x *= -1;
-	    prev.x *= -1;
+	    prev.x = (prev.x*-1) + 2*p; curr.x = (curr.x*-1) + 2*p;
+	    prev.y += 1; curr.y += 1;
+	    draw_line(img, line_color, prev, curr);
+	    prev.y -= 1; curr.y -= 1;
+	    prev.x = (prev.x*-1) + 2*p; curr.x = (curr.x*-1) + 2*p;
 	}
 	prev = curr;
-	curr.x = x;
-	curr.y = a*(x*x) + b*x + c;
+	curr.x = round(x + WIDTH/2);
+	curr.y = round(a*(x*x) + b*x + c + HEIGHT/2);
 	x++;
     }
 }
@@ -100,19 +103,18 @@ void draw_parabola(Image img, float a, float b, float c) {
 void generate_plot_bg(Image img) {
     Color bg_color = { 40,40,40 };
     Color axis_color = { 90,90,90 };
-    fill(img, bg_color);
+    image_fill(img, bg_color);
     // draw axis
-    Point left = { WIDTH/2*-1, 0 };
-    Point right = { WIDTH/2-1, 0 };
-    Point top = { 0, HEIGHT/2-1 };
-    Point bot = { 0, HEIGHT/2*-1 };
-    line(img, axis_color, left, right);
-    line(img, axis_color, bot, top);
+    Point left = { 0, HEIGHT/2 };
+    Point right = { WIDTH, HEIGHT/2 };
+    draw_line(img, axis_color, left, right);
+    Point top = { WIDTH/2, 0 };
+    Point bot = { WIDTH/2, HEIGHT };
+    draw_line(img, axis_color, top, bot);
 }
 
 // https://en.wikipedia.org/wiki/Netpbm#File_formats
-void save_as_ppm(Image img, const char* file_name)
-{
+void save_as_ppm(Image img, const char* file_name) {
     FILE *file = fopen(file_name, "wb");
     assert(file != NULL);
     // ppm header
@@ -125,8 +127,11 @@ void save_as_ppm(Image img, const char* file_name)
 }
 
 int main() {
-    Image out; 
+    Color (*out)[WIDTH] = malloc(sizeof(Image));
+
     generate_plot_bg(out);
-    draw_parabola(out, 1.0f/50, 0, 0);
+    draw_quadratic(out, 1.0/50, 2, 0);
     save_as_ppm(out, "test.ppm");
+
+    free(out);
 }
